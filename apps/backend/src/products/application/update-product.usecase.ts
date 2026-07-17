330f20e6d7acc312e244b6f7096d5a04b7bc0406
Davi S Moraes <davi.moraes@namu.com.br>
2026-07-16 22:55:44 -0300

    feat: implement UpdateProductUseCase (green)

---
+++ apps/backend/src/products/application/update-product.usecase.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Price } from '../domain/price.vo';
+import { ProductRepository } from '../domain/product.repository';
+import { ProductNotFoundError } from '../domain/product.errors';
+
+export class UpdateProductInput {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+@Injectable()
+export class UpdateProductUseCase {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(input: UpdateProductInput) {
+    const product = await this.productRepository.findById(input.id);
+    if (!product) throw new ProductNotFoundError(input.id);
+
+    const price = new Price(input.price);
+    product.update(input.name, price, input.stock);
+    await this.productRepository.save(product);
+    return product;
+  }
+}
